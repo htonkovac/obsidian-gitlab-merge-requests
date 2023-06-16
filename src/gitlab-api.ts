68c7ed12d5d4c33dd93d12b8d33d8f42ee0ef082
Hrvoje Tonkovac <hrvoje.tonkovac@toptal.com>
2023-06-16 21:22:10 +0200

    cache

--- src/gitlab-api.ts
+++ src/gitlab-api.ts
@@ -1,7 +1,28 @@
 import { requestUrl, RequestUrlParam, RequestUrlResponse } from 'obsidian';
 import { GitlabIssuesSettings } from './settings';
 import { GitlabMergeRequests } from './gitlab-mr'
+const ms = require('ms')
+
+class Cache {
+	cache: Array<GitlabMergeRequests> = []
+	cacheUpdateTime: number = 0
+
+	setCache(mrs: Array<GitlabMergeRequests>) {
+		this.cache = mrs
+		this.cacheUpdateTime = Date.now()
+	}
+
+	getCache(): Array<GitlabMergeRequests> | null{
+		if (this.cacheUpdateTime + ms('3000') < Date.now()) {
+			return null
+		}
+
+		return this.cache
+	}
+}
+
 export default class GitlabApi {
+	static cache: Cache = new Cache()
 
 	// refactor to async/await
 	static load<T>(url: string, gitlabToken: string): Promise<T> {
@@ -20,17 +41,22 @@ export default class GitlabApi {
 			});
 	}
 
-	static getAllMRs(settings: GitlabIssuesSettings): Promise<Array<GitlabMergeRequests>> {
-
+	static async getAllMRs(settings: GitlabIssuesSettings): Promise<Array<GitlabMergeRequests>> {
 		const date = new Date();
 		date.setDate(date.getDate() - 30);
 
 		const url = `${settings.gitlabUrl}/api/v4/merge_requests?created_after=${date.toISOString()}`;
-		return GitlabApi.load<Array<GitlabMergeRequests>>(url, settings.gitlabToken)
+		const mrs = await this.load<Array<GitlabMergeRequests>>(url, settings.gitlabToken)
+
+		this.cache.setCache(mrs)
+		return mrs
 	}
 
 	static async getMR(settings: GitlabIssuesSettings, mrId: string): Promise<GitlabMergeRequests> {
-		const all_mrs = await this.getAllMRs(settings)
+		let all_mrs = this.cache.getCache()
+		if (all_mrs == null) {
+			all_mrs = await this.getAllMRs(settings)
+		}
 
 		const mr = all_mrs.find((mr: GitlabMergeRequests) => {
 			return mr.references.full == mrId
